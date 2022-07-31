84ef945f6ade9eb5ebe64cb089a3125144a0e656
harsha <harshavardhanreddyb07@gmail.com>
2022-08-01 00:46:05 +0530

    header component added

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './MyComponents/Header'; //we don't have to use {} as export is default
+import {Todos} from './MyComponents/Todos'; //we have to use {} as export is not default for this
+import {Footer} from './MyComponents/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <>    //html content should be embeded in something like id, className  if not we have to embed that in <>  </>
+    // <h3>Todo</h3>
+    // <p>My app works</p>
+    // </>
+
+   <>
+      <Header/>
+      <Todos/>
+      <Footer/>
+   </>
+
   );
 }
 
